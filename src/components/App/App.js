60dc1289f1ed1fcdcd7d70ec4b5c100ffff3dc65
Tatyana Karpova <tatyana.pushkareva.1997@mail.ru>
2023-07-11 20:14:07 +0200

    feat: add routes and fonts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page'>
+      <Routes>
+        <Route exact path='/' component={Main} />
+        <Route path='/movies' component={Movies} />
+        <Route path='/saved-movies' component={SavedMovies} />
+        <Route path='/profile' component={Profile} />
+        <Route path='/signin' component={Login} />
+        <Route path='/signup' component={Register} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
